6d20c56c58283499826f589d8acd55f911ee5d85
Carol Wargo <Carolwargo@gmail.com>
2023-11-05 09:38:20 -0500

    Updated backend to test

--- server/models/User.js
+++ server/models/User.js
@@ -23,7 +23,7 @@ const userSchema = new Schema({
 });
 
 // set up pre-save middleware to create password
-userSchemaSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
