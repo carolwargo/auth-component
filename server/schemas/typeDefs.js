9457dcfd27aae78bc0a1d57f98156bf5a63ec932
Carol Wargo <Carolwargo@gmail.com>
2023-11-05 10:27:59 -0500

    update typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,30 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID
     name: String
     email: String
     password: String
-}
+  
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     user: User
-}
+  }
 
-type Query {
-    users: [User]!
-   user(userId: ID!): User
-}
-type Mutation {
+  type Query {
+  user: [User]!
+    user(userId: ID!): User
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: User
+  }
+
+  type Mutation {
     addUser(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    removeUser(userId: ID!): User
-}
-`
-;
+`;
 
-module.export =typeDefs;
+module.exports = typeDefs;
+
