99ade4943a1b05ed6d689ab7c0a16f89a971573f
Carol Wargo <Carolwargo@gmail.com>
2023-11-05 06:11:27 -0500

    added backend

---
+++ server/schema/resolvers.js
@@ -0,0 +1,46 @@
+const {AuthenticationError} = require('apollo-server-express');
+const {User} = require('./models');
+const {signToken} = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async()=> {
+            return User.find();
+        },
+
+        user: async(parent, {userId}) => {
+            return User.findOne({_id:userId});
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+          const user = await User.create({ name, email, password });
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+
+        login: async (parent, {email, password, }) => {
+            const user = await User.findOne({email});
+
+            if (!user)  {
+                throw new AuthenticationError('No User with this email found!')
+            }
+
+            const correctPw= await user.isCorrectPassword(password);
+            
+            if (!correctPw) {
+                throw new  AuthenticationError('Incorrect Password')
+            }
+
+            const token = signToken(user);
+            return {token, user};
+        }, 
+
+        removeUser: async (parent, {userId}) => {
+            return User.findOneAndDelete({_id: userId});
+        },
+    },
+    };
+    module.exports = resolvers
---
+++ server/schema/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    users: [User]!
+    profile(profileId: ID!): User
+}
+type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    removeUser(userId: ID!): User
+}
+`
+;
+
+module.export =typeDefs;
