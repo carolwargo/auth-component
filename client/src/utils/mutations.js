c0c6bcc7651f582449ec3c19d3594a5c64525141
Carol Wargo <Carolwargo@gmail.com>
2023-11-05 10:07:09 -0500

    added login mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -20,3 +20,14 @@ export const DELETE_USER = gql`
     }
   }
 `;
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
