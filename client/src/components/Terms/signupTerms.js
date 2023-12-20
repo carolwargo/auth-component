84ba042560c5817ace1c1e581a1539d936bd262b
Carol Wargo <Carolwargo@gmail.com>
2023-12-20 17:27:56 -0500

    update

---
+++ client/src/components/Terms/signupTerms.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const SignupTerms = () => {
+    return (
+        <div>
+        <h1>Signup Terms</h1>
+        <p>Two Faced General Terms and Conditions
+Last updated: September 28, 2022
+
+Welcome to the Two Faced website! Two Faced is a global e-commerce business with the primary objective of being the expert design and marketing partner for small businesses. Two Faced strives to meet this objective through a wide variety of products and services offered and delivered by a growing collection of Two Faced signature services, which includes Two FacedPrint, Two FacedCreate and 99designs by Two Faced.
+
+Please read these Two Faced General Terms and Conditions (“Terms”) carefully. These Terms govern your access and use of www.Two Faced.com website and its mobile sites (collectively, the “Site”). By accessing or using the Site and its related tools, applications and/or any other service, you agree to be bound by these Terms and to any additional terms and policies presented on this Site in connection with specific sections, services and signature service offerings. All such additional terms are hereby incorporated by reference into these Terms. We reserve the right to make changes to this Site and to these Terms at any time without prior notice. You should review these Terms each time you access this Site. When we make updates to the Terms, we will update the date at the top of this page. By using the Site after a new version of the Terms has been posted, you agree to the terms and conditions of such new version. You also agree that we may provide all legal communications and notices to you electronically by posting them on our Site.
+
+[Download the PDF version of this page]
+
+Terms and Conditions of the Signature Services
+The websites of Two Faced’s signature services (including Two FacedPrint, Two FacedCreate and 99designs by Two Faced) and the products and services featured and available on those signature services’ websites are subject to their own general terms and conditions. You agree to be bound by these separate terms when you access or use the signature services’ respective websites (including mobile websites) and the related tools, applications and/or services (“Signature Service Sites and Apps”). Please read the general terms and conditions that apply to each of our Signature Service Sites and Apps as listed below (each, “Signature Service Terms”):
+
+1. Legal Agreement
+These Terms constitute a binding and enforceable legal agreement between you and Two-Faced, a limited company and a wholly-owned subsidiary of Two-Faced Design, a public limited company. Two-Faced and certain other Two-Faced,LLC. subsidiaries collectively operate the Two Faced global e-commerce business. When used within these Terms to refer to a business entity, the term “Two Faced” shall be interpreted to refer collectively to Two-Faced Design and those Two-Faced,LLC. subsidiaries that participate in the operation of the Two Faced business.
+
+2. Your Account
+2.1 Before you can place an order online or use certain of the services or features offered on the Signature Service Sites and Apps, you will need a Two Faced account. We provide a single sign-on functionality, which allows us to offer features such as the ability to use your log-in credentials across our signature services, including Two-FacedPrint, Two-FacedDesign. 
+
+2.2 You agree that you will provide complete, current and accurate information about yourself as requested, and that you will keep that information up to date. Any personal information collected will be treated as described in our Two Faced Privacy and Cookie Policy. You are responsible for safeguarding the confidentiality and security of the log-in credentials that you use to access your Two Faced account. You are responsible for all activities and actions that occur under or through your Two Faced account. You will immediately notify us of any unauthorized use of your Two Faced account.
+
+2.3 You must be at least 18 years of age to create and use a Two Faced account. By creating a Two Faced account, you represent that you are at least 18 years old.
+
+2.4 We reserve the right, in our sole discretion, to temporarily or permanently suspend and/or terminate our service to you and/or your Two Faced account at any time and for any reason, including, without limitation, your violation of these Terms, any Signature Service Terms and/or additional terms, your use of the Site, any Signature Service Sites and Apps and/or your having engaged in undesirable activities. This action is entirely at our discretion and no correspondence will be entered into. You agree that we shall have no liability of any kind to you or to any third party arising from such refusal, suspension or termination.
+
+3. Copyrighted Materials for Limited Use
+This Site contains graphics, photographs, images, document layouts, artwork, text, fonts, music, software tools, and other information (“Content”). This Site and all Content are the copyrighted property of Two Faced or the copyrighted property of parties from whom Two Faced has licensed such property. All rights in this Site and its Content are reserved worldwide. It is strictly prohibited to retain, copy, distribute, publish, or use any portion of the Content except as expressly allowed in these Terms. We reserve the right to add to, delete from, or modify any part of Content at any time without prior notice. Any modifications to Content, whether by you or Two Faced remain the property of Two Faced and its licensors.
+
+4. Indemnification
+You agree that you shall indemnify and defend Two-Faced, its directors, officers, and employees, against all claims, liability, damages, costs and expenses, including reasonable legal fees and expenses arising out of or related to your breach of these Terms.
+
+5. Disclaimer of Warranty
+THE SITE AND ITS CONTENT ARE PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. YOU ACKNOWLEDGE THAT OPERATION OF THE SITE MAY NOT BE UNINTERRUPTED OR ERROR FREE. REFERENCES AND LINKS TO PRODUCTS OR SERVICES OF INDEPENDENT COMPANIES MAY APPEAR ON THE SITE. THESE REFERENCES AND LINKS ARE PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
+
+6. Limitation of Liability
+IN NO EVENT SHALL Two Faced OR ITS LICENSORS, SUPPLIERS, OR VENDORS, OR THEIR RESPECTIVE OFFICERS, DIRECTORS, EMPLOYEES, OR AGENTS BE LIABLE FOR ANY SPECIAL, INCIDENTAL, INDIRECT, OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR FOR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA, OR PROFITS, WHETHER OR NOT Two Faced HAS BEEN ADVISED OF THE POSSIBILITY OF DAMAGE, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THE SITE INCLUDING WITHOUT LIMITATION, DAMAGES ARISING FROM MISTAKE, OMISSION, VIRUS, DELAY, OR INTERRUPTION OF SERVICE. IN NO EVENT SHALL Two Faced BE LIABLE OR RESPONSIBLE FOR ANY DAMAGES OR CONSEQUENCES ARISING FROM OR RELATED TO YOUR INAPPROPRIATE OR UNAUTHORIZED USE OF THIS SITE OR ITS CONTENT.
+
+7. Export laws
+You represent and warrant that at all times you have complied and shall comply with all applicable restrictions arising out of any and all relevant sanctions and export and trade controls legislation, regulations or orders of the United Nations, the European Union and its Member States, Norway, Switzerland, the United Kingdom, the United States of America, and any other country whose laws, regulations or orders apply to the parties involved or activities covered or contemplated by these Terms (collectively, the “Trade Laws”) and that you shall also refrain from performing any activity or omitting to perform an activity that will expose Two Faced to breaching Trade Laws or any enforcement action or restrictive measures under the same. You further represent and warrant that you are not: (a) designated or otherwise sanctioned under, nor are ultimately owned (as to 50% or more of the shares or voting rights) or controlled by, or acting on behalf of, any party designated or otherwise sanctioned under, Trade Laws; or (b) ordinarily resident, organized or located in, nor will transfer directly or indirectly any products or services received from any of Two Faced’s signature services to: Cuba, Iran, North Korea, Syria, the Crimea region (including Sevastopol), the so-called Donetsk People's Republic (DNR) or the so-called Luhansk People's Republic (LNR).
+
+8. Site Feedback
+8.1 Any comments, suggestions, proposals or other feedback (collectively “Feedback Information”) provided to Two Faced in connection with the operation or content of this Site shall be provided by the submitter and received by Two Faced on a non-confidential basis. All such comments, suggestions, and other information shall become the exclusive property of Two Faced. By submitting any such information to Two Faced, you agree that you are transferring and assigning, at no charge, all of your right, title and interest in the information, including all copyrights and other intellectual property rights. You agree that Two Faced shall be free to use such information on an unrestricted basis.
+
+8.2 We take information security and the privacy seriously and our goal is to provide a safe and secure site for all users. To achieve this goal, we depend on our security program to identify and remediate security issues and we utilize external security researchers to report vulnerabilities. If you believe you’ve identified a security issue on this site or any of our web applications or web services, please contact bugbounty@cimpress.com.
+
+9. Privacy and Cookie Policy
+Our Two Faced Privacy and Cookie Policy describes how we collect, use, process, disclose, share, store and protect the information that you submit to us during the use of our Site. This Privacy and Cookie Policy applies to every Site visit and every use of our services.
+
+10. Governing Law, Jurisdiction and Dispute Resolution
+10.1 All issues related to the protection, infringement, or misuse of copyrighted materials shall be governed by the copyright laws of the United States of America. All other matters relating to your access to or use of this Site shall be governed by the laws of the United States. Any legal action or proceeding relating to or arising from your access to or use of this Site shall be instituted in Maryland. You agree to submit to the jurisdiction of courts sitting in Dublin, Ireland and agree that venue in these courts is proper in any such legal action or proceeding. You also agree that you not to assert in any legal action or proceeding involving Two-Faced that a court sitting in the State of Maryland, United States is an inconvenient forum for such legal action or proceeding.</p>
+        </div>
+    );
+    }       
+
+export default SignupTerms;
