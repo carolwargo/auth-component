e7e681abddfae3647282d3fb78f27391a3f483ec
Carol Wargo <Carolwargo@gmail.com>
2023-12-28 11:39:02 -0500

    added progress bar for upload

---
+++ client/src/components/Terms/privacyPolicy.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PrivacyPolicy = () => {
+
+    return (
+        <div className="container">
+            <h1>Cookies and Privacy Policy</h1>
+            <p style={{fontSize:'12px'}}>last updated on: 12/21/2023</p>
+<h5>This “US Notice” is applicable to residents of certain US states that have their own laws and regulations regarding data privacy, such as California, Virginia, Colorado, Connecticut, Utah or Nevada. It describes how we collect, use, disclose, and otherwise process Personal Information, and which rights may be available to residents of these US states upon the effective dates of the respective US state laws and regulations (collectively, “US State Laws”).
+</h5>
+<p>The terms of this US Notice supplements and should be read in conjunction with our Two-Faced Privacy and Cookie Policy.</p>
+<p>
+Depending on your place of residence, and subject to the applicable law in that State, some parts of this US Notice will not be applicable to you.
+<br></br>
+<b>Your Personal Information:</b>
+You can learn about the categories of Personal Information we collect from you, the sources of that information, the purposes for collecting and using that information, and what types of third-party service providers we may share that information with, by reading Sections 3 to 7 of the Two-Faced Privacy and Cookie Policy.
+<br></br>
+<b>Your Privacy Rights:</b>
+Depending on your place of residence, and subject to certain limitations under the applicable US State Laws, you may exercise the following rights in relation to your Personal Information:
+<br></br>
+<ul>
+    <li>
+        <b>Access:</b> The right to confirm whether or not we are processing your Personal Information, and request we provide you with the following information (including in a portable, and, to the extent technically feasible, readily usable format):
+    </li>
+    <li>
+        <b>Correct: </b>The right to correct or update inaccurate or incomplete Personal Information, taking into account the nature of the Personal Information and the purposes of the processing.
+    </li>
+    <li>
+        <b>Deletion: </b>The right to request we delete the Personal Information we have collected from you, with some exceptions.
+    </li>
+    <li>
+        <b>Opt Out: </b>If applicable, you may have the right to tell companies not to sell or share your Personal Information, or to opt-out of the processing of Personal Information for purposes of targeted advertising , sale, or profiling (as defined under the applicable US State Laws).
+    </li>
+    <li>
+        <b>Limit: </b>The right to limit the use or disclosure of sensitive personal information. Please note that we do not collect or process sensitive Personal Information for the purpose of inferring characteristics about you.
+    </li>
+    <li>
+        <b>Portability: </b>The right to request a copy of your Personal Information in electronic format and the right to transmit that Personal Information for use in another party’s service.
+    </li>
+    <li>
+        <b>Right to be informed: </b>The right to be informed about the categories of Personal Information collected and the purposes for which such Personal Information is used and shared.
+    </li>
+   
+    <li>
+        <b>Nondiscrimination:</b> The right not to be discriminated against for exercising any of these rights.
+</li>
+<li>
+    <b>Appeal:</b> Where applicable, you may have the right to appeal our refusal to take action on, or respond to, a verified consumer request. Upon receipt of our denial or refusal to take action on, or respond to, a verified consumer request, you may, within a reasonable period of time from the refusal, submit a request to appeal our decision by contacting through email.
+</li>
+<li>
+    <b>
+    How to Exercise Your Privacy Rights:
+    </b>
+You, or an authorized agent (where applicable), can exercise these rights with respect to one or more of our signature services as provided under the Contact Us section below and to the extent applicable to you under the respective US State Laws. In order to exercise your rights, we may have to verify your identity. We will do this by asking you to provide us with certain information we already have to confirm your identity. This can include your contact information, account number, or purchase history details. Authorized agents, where applicable, will be required to provide proof of their authorization and we may also require that you directly verify their identity and the authority of the authorized agent.
+<br></br>We recognize and support the Global Privacy Control (“GPC”) standard. As such, if you enabled your browser or device to broadcast a GPC signal, we will automatically process it upon receival as a valid request to opt out of the sale and sharing of your Personal Information. If you log into your account, your preference will be associated with your Two-Faced account, maintaining your opt out preference when using a different device or browser. However, if you are not logged into your account, your preference will only be associated with that one browser.
+</li>
+<li>
+    <b>
+    Special Disclosures for California Residents<br></br>
+Shine the Light:
+    </b>
+    As a California resident, you may request, once a year and free of charge, a list of the third parties to whom we have disclosed Personal Information (if any) for their own direct marketing purposes. See the Contact Us section below for how to make this request. However, note we do not share your Personal Information with third parties for their own marketing purposes without your express consent. Accordingly, you can prevent the type of sharing by withholding consent or opting out of sharing of Personal Information as further described on the Notice of Right to Opt Out section below.
+</li>
+<li>
+    <b>Notice of Right to Opt Out:</b>
+    We do not sell your Personal Information in exchange for money. However, like many ecommerce companies, we do share your Personal Information with third parties, such as our advertising partners to help show relevant ads based on your interests. If you would like to learn more, or wish to opt-out of the sale or sharing of your Personal Information, please see the Contact Us section below. Please note that we do not knowingly sell or share the Personal Information of individuals under the age of 16 years old.
+
+</li>
+<li>
+    <b>Notice of Financial Incentive</b>
+    In some instances, we may offer various types of incentives in exchange for the collection of your Personal Information, such as discounts, promotional codes, prices as part of a sweepstake entry or contest, participation in surveys, exclusive offers when you sign up to receive our marketing emails and other programs. The amount and terms of such incentives will be presented to you at the time of the offer or program. When you participate in or sign up for one of these offers or programs, we collect Personal Information from you, such as identifiers like your name, email address and/or phone number.
+    <br></br>
+    Because these activities involve the collection of Personal Information as well as the offering of various benefits, they might be interpreted as providing a “financial incentive” under California law. To the extent that we derive value from the collection of your Personal Information, we reasonably estimate the value of the financial incentive to be equal to the value we receive from the Personal Information collected. We estimate the value of a consumer’s Personal Information by directly relating it to the expenses which might be incurred in the provision of each incentive and the collection, storage, and use of such Personal Information in the operation of our business.
+    <br></br>
+    Participation in our financial incentives is voluntary. You may opt into a financial incentive by following the sign-up or participation instructions provided, and you have the ability to opt out or withdraw at any time by unsubscribing from our marketing communications or terminating the participation in the sweepstake or contest prospectively by following the instructions in the applicable terms.
+    <br></br>
+    We do not offer financial incentives that are unjust, unreasonable, coercive, or usurious in nature. If you have any questions about our financial incentives, please contact us as indicated under the Contact Us section below.
+</li>
+</ul>
+
+<ul>
+    <li><li>
+Special Disclosures for Virginia, Colorado, Connecticut and Utah Residents
+Unless specifically stated, we do not sell Personal Information to third parties, nor do we process your Personal Information for the purpose of profiling in furtherance of decisions that produce legal or similarly significant effects. However, we do process your Personal Information for targeted advertising purposes. If you would like to learn more, or opt out of the sale or processing of your Personal Information for targeted advertising purposes, please see the Contact Us section below.
+</li>Special Disclosures for Nevada Residents
+Nevada law gives Nevada residents the right to request that a company not sell their Personal Information for monetary consideration to certain other parties.  The right to opt out of the sale of your Personal Information applies even if your Personal Information is not currently being sold.  If you are a Nevada resident and wish to exercise this right, please contact us as indicated under the Contact Us section below.
+</li>
+</ul>
+
+Contact Us
+If you would like to learn more or wish to exercise your rights under the applicable US State Laws with respect to one or more of our signature services, or If you have any questions about this Notice, please contact our privacy team by <Link to='/contact'>contacting us</Link>.</p>
+        </div>
+    )
+}
+
+export default PrivacyPolicy;
